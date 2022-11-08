20657c71cfb990025d48fdd586c3fb11cb1495df
Gabriel Octavian Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-08 10:51:57 +0100

    He añadido @Getter y @Setter de lombok y he eliminado el codigo redundante

--- src/main/java/cine/Pelicula.java
+++ src/main/java/cine/Pelicula.java
@@ -1,11 +1,16 @@
 package cine;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Pelicula {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,52 +20,4 @@ public class Pelicula {
     private String año;
     private String duracion;
     private String categoria;
-
-    public String getAño() {
-        return año;
-    }
-
-    public void setAño(String año) {
-        this.año = año;
-    }
-
-    public String getDuracion() {
-        return duracion;
-    }
-
-    public void setDuracion(String duracion) {
-        this.duracion = duracion;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String genero) {
-        this.categoria = genero;
-    }
 }
