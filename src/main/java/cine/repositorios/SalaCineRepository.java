905fba300d52f87a5bea01c29f036d160ebaaf21
Gabriel Octavian Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-08 12:40:29 +0100

    He creado el repositorio de las salas de cine

---
+++ src/main/java/cine/repositorios/SalaCineRepository.java
@@ -0,0 +1,8 @@
+package cine.repositorios;
+
+import cine.pojos.SalaCine;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalaCineRepository extends CrudRepository<SalaCine, Integer> {
+
+}
