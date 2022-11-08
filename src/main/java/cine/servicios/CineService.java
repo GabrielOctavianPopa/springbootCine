f07731f76c4260fea30e32f5ed05220c65077310
Gabriel Octavian Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-08 12:42:28 +0100

    He movido el Servicio del cine a un nuevo package y he añadido la funcionalidad de las salas de cine

--- src/main/java/cine/servicios/CineService.java
+++ src/main/java/cine/servicios/CineService.java
@@ -1,5 +1,9 @@
-package cine;
+package cine.servicios;
 
+import cine.pojos.Pelicula;
+import cine.repositorios.PeliculasRepository;
+import cine.pojos.SalaCine;
+import cine.repositorios.SalaCineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +13,8 @@ import java.util.ArrayList;
 public class CineService {
     @Autowired
     public PeliculasRepository peliculasRepository;
+    @Autowired
+    public SalaCineRepository salaCineRepository;
 
     public void addPelicula(Pelicula pelicula) {
         peliculasRepository.save(pelicula);
@@ -18,5 +24,15 @@ public class CineService {
         ArrayList<Pelicula> peliculas = new ArrayList<>();
         peliculasRepository.findAll().forEach(peliculas::add);
         return peliculas;
+    }
+
+    public void addSala(SalaCine sala) {
+        salaCineRepository.save(sala);
+    }
+
+    public ArrayList<SalaCine> getAllSalas() {
+        ArrayList<SalaCine> salas = new ArrayList<>();
+        salaCineRepository.findAll().forEach(salas::add);
+        return salas;
     }
 }
