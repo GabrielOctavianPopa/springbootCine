8523377e9de79b42145e15912f99054a3652dcfc
Gabriel Octavian Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-08 10:51:01 +0100

    He cambiado el codigo y añadido comentarios

--- src/main/java/cine/JsonCreator.java
+++ src/main/java/cine/JsonCreator.java
@@ -3,19 +3,26 @@ package cine;
 import com.github.javafaker.Faker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JsonCreator {
     public static void main(String[] args) {
         Faker faker = new Faker();
         ArrayList<String> peliculas = new ArrayList<>();
         String titulo, director, año, duracion, categoria;
-        for (int i = 0; i <= 2; i++) {
+
+        Scanner sc = new Scanner(System.in);
+        int numeroPeliculas = sc.nextInt();
+
+        for (int i = 0; i <= numeroPeliculas; i++) {
+            // Generar datos aleatorios
             titulo = faker.book().title();
             director = faker.book().author();
             año = String.valueOf(faker.number().numberBetween(1800, 2022));
             duracion = String.valueOf(faker.number().numberBetween(60, 240));
             categoria = faker.book().genre();
-            //add the elements to the arraylist with the format of the json
+
+            //añadir pelicula a la lista con el formato de un json correcto
             peliculas.add("{\n" +
                     "    \"titulo\": \"" + titulo + "\",\n" +
                     "    \"director\": \"" + director + "\",\n" +
@@ -24,10 +31,7 @@ public class JsonCreator {
                     "    \"categoria\": \"" + categoria + "\"\n" +
                     "}");
         }
-
-        //return each pelicula in the arraylist as a JSON string as a new line
+        //Recorrer el arraylist de peliculas y mostrarlas por pantalla
         peliculas.forEach(System.out::println);
-
-
     }
 }
