0b95f6ca1aeffae11af332adef867f275fe51899
Gabriel Octavian Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-09 09:33:54 +0100

    Movido y editado los css y scripts

---
+++ src/main/resources/static/javascript/custom.js
@@ -0,0 +1,38 @@
+function addMovie() {
+    var ititulo = document.getElementById("titulo").value;
+    var idirector = document.getElementById("director").value;
+    var iaño = document.getElementById("año").value;
+    var iduracion = document.getElementById("duracion").value;
+    var igenero = document.getElementById("genero").value;
+
+    fetch("http://localhost:8080/cine/add-movie", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            titulo: ititulo,
+            director: idirector,
+            año: iaño,
+            duracion: iduracion,
+            genero: igenero
+        })
+    })
+        .then(response => response.text())
+        .then(result => notify(result))
+}
+
+function clean() {
+    <!-- Delete the contents from the form -->
+    document.getElementById("titulo").value = "";
+    document.getElementById("director").value = "";
+    document.getElementById("año").value = "";
+    document.getElementById("duracion").value = "";
+    document.getElementById("genero").value = "";
+    notify("borrados")
+}
+
+function notify(message) {
+    document.getElementById("errormsg").innerHTML = message;
+}
